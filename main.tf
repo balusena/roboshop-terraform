009621a57fb6261317e21ebe5cd668d6378da455
balusena <balusena12@gmail.com>
2024-01-09 21:43:51 +0000

    roboshop terraform files

--- main.tf
+++ main.tf
@@ -1,21 +1,24 @@
 module "vpc" {
   source      = "git::https://github.com/balusena/tf-module-vpc.git"
+
   for_each    = var.vpc
   cidr_block  = each.value["cidr_block"]
   subnets     = each.value["subnets"]
 
-  env  = var.env
-  tags = var.tags
+  env            = var.env
+  tags           = var.tags
   default_vpc_id = var.default_vpc_id
+
 }
 
 module "app_server" {
-  source = "git::https://github.com/balusena/tf-module-app.git"
+  source    = "git::https://github.com/balusena/tf-module-app.git"
+
   env       = var.env
   tags      = var.tags
   component = "test"
-#  subnet_id = module.vpc["subnet_ids"]["app"]["subnets_ids"][0]
-  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "app", null), "subnet_ids", null)[0]
-  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "app", null), "subnet_ids",
+    null)[0]
+  vpc_id    = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
 }
 
